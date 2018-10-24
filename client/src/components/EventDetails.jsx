0546455b8c339f58a9b1cfd8e04f401233bc2397
Kristine <kristinetrona@gmail.com>
2018-10-24 11:01:12 +0200

    adjustments made to event details page

--- client/src/components/EventDetails.jsx
+++ client/src/components/EventDetails.jsx
@@ -4,9 +4,17 @@ export const EventDetails = (props) =>{
   return (
     <div className="event-list-wrapper">
       {props.events.map(event => 
-        <div className = "event-info">
-          <img className="event-image" src={event.image} alt="game-image"/>
-          {event.title}
+        <div className = "event-info" key={event.id}>
+          <img className="event-image" src={event.image} alt="artistic visualization of the game"/>
+          <div>
+            <h4>{event.title}</h4>
+            {event.description}
+          </div>
+          <div className="event-time-place">
+            Date: {event.date} <br/>
+            Time: {event.time} <br/>
+            Place: {event.place}
+          </div>
         </div>)}
     </div>
   )
