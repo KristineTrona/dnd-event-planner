ebe21f0fa29b107d2bf6109a1a1046bc3a2b19e5
Kristine <kristinetrona@gmail.com>
2018-11-19 12:42:48 +0100

    switched to sass

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,43 +1,14 @@
-"use_strict"
-const gulp=require('gulp');
-const browserSync=require('browser-sync').create();
-const sass=require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 
-gulp.task('sass',function(){
-
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss'])
+gulp.task('sass', function(){
+  return gulp.src('./src/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest("src/css"))
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest('./src/'));
 });
 
-gulp.task('js',function(){
-  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js','node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
-  .pipe(gulp.dest("src/js"))
-  .pipe(browserSync.stream());
+gulp.task('bootstrap', function(){
+  return gulp.src('./node_modules/bootstrap/scss/bootstrap.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./src/'));
 });
-
-
-
-gulp.task('serve',['sass'],function(){
-      browserSync.init({
-        server:"./src"
-      });
-
-      gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss'],['sass']);
-      gulp.watch("src/*.html").on('change',browserSync.reload);
-
-});
-
-gulp.task('fonts',function(){
-  return gulp.src('node_modules/font-awesome/fonts/*')
-  .pipe(gulp.dest("src/fonts"));
-});
-
-gulp.task('fa',function(){
-  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-  .pipe(gulp.dest("src/css"));
-});
-
-
-gulp.task('default',['js','serve','fa','fonts']);
